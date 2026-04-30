045d474b861bc82670dbdead338b99cabe7308de
Sumaya <sumayasumi663@gmail.com>
2026-04-30 15:17:46 +0530

    changed

--- solar_app_software/app.py
+++ solar_app_software/app.py
@@ -213,7 +213,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id         = db.Column(db.Integer, primary_key=True)
     username   = db.Column(db.String(80),  unique=True, nullable=False)
-    email      = db.Column(db.String(120), unique=True, nullable=False)
+    email      = db.Column(db.String(120), nullable=False)
     phone=db.Column(db.String(20),unique=True,nullable=False)
     password   = db.Column(db.String(512), nullable=False)   
     full_name  = db.Column(db.String(120), nullable=False)
