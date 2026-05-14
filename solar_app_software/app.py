bcf5a82c88c10316865e4a50e6281911bec22d56
Sumaya <sumayasumi663@gmail.com>
2026-05-14 11:54:14 +0530

    db connection setup fo aws

--- solar_app_software/app.py
+++ solar_app_software/app.py
@@ -30,7 +30,8 @@ from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_wtf.csrf import generate_csrf
-
+from dotenv import load_dotenv
+load_dotenv()
 
 DCR_SUBSIDY_AMOUNT = 78000
 
