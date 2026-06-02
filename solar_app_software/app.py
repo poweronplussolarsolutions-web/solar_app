1930d09afb04e1c45862f2746c1763ba4680823a
Sumaya <sumayasumi663@gmail.com>
2026-06-02 15:57:24 +0530

    changed roof type values

--- solar_app_software/app.py
+++ solar_app_software/app.py
@@ -293,7 +293,7 @@ class Project(db.Model):
     materials        = db.relationship('Material',         backref='project', lazy=True)
     assignments      = db.relationship('WorkerAssignment', backref='project', lazy=True)
     loan_subtype     = db.Column(db.Enum('Assisted','Self'), nullable=True)
-    roof_type=db.Column(db.Enum('F','C','S'),nullable=True)
+    roof_type=db.Column(db.Enum('Flat','Sheet','Slope'),nullable=True)
     panel_items      = db.relationship('PanelItem', backref='project', lazy=True, cascade='all,delete-orphan')
     extra_materials  = db.relationship('ExtraMaterial', backref='project', lazy=True, cascade='all,delete-orphan')
 
